d6bf928d1b52ea041217e51dc3e5e7ae63e32bf6
Grifthegnome <grifbd@aol.com>
2024-01-23 00:40:28 -0800

    Added Propper Hookup For Bloody Water - Added Wound Bleed FX - Fixed Eye Position Bounce Bug - Cleaned Up Code
    Added Propper Hookup For Bloody Water
    1. Water Color Is now factored into particle color,
    2. The particle only plays when the bleed target has feet in water or is swimming.
    
    Added Wound Bleed FX
    1. Added Wound Bleed FX which is just a bit of blood that drops to the ground from the hit point.
    
    Fixed Eye Position Bounce Bug
    1. For some silly reason I was using .Mul on the EntityAgent eye position, this was causing the player view to bounce when hit with a weapon.
    
    Cleaned Up Code
    1. Deleted commented out code.
    2. Moved all networking concepts into BrutalBroadcast.cs

--- mods-dll/brutalstory/src/BloodFX.cs
+++ mods-dll/brutalstory/src/BloodFX.cs
@@ -12,6 +12,7 @@ using System.Drawing;
 using BrutalStory;
 using Vintagestory.API.Datastructures;
 using Vintagestory.API.Server;
+using Vintagestory.ServerMods;
 
 namespace BrutalStory
 {
@@ -25,11 +26,13 @@ namespace BrutalStory
         static int emptyColor = ColorUtil.ColorFromRgba(0,0,0,0);
 
         static BrutalStandardFxData bloodSprayProperties;
+        static BrutalStandardFxData woundBleedProperties;
         static BrutalBloodyWaterFxData bloodyWaterProperties;
 
         public static void Init()
         {
             //Create Bloodspray Settings
+            //This is blood that sprays from the wound.
             Vec3f bloodSprayMinVel = new Vec3f(5.0f, 5.0f, 5.0f);
             Vec3f bloodSprayMaxVel = new Vec3f(15.0f, 15.0f, 15.0f);
             int bloodSprayColorCode = bloodColorDefaultCode;
@@ -37,7 +40,7 @@ namespace BrutalStory
             int bloodSprayMinQuantity = 5;
             int bloodSprayMaxQuantity = 20;
 
-            float bloodSprayMinVelocityScale = 2;
+            float bloodSprayMinVelocityScale = 1;
             float bloodSprayMaxVelocityScale = 8;
 
             float bloodSprayLifeLength = 10f;
@@ -58,6 +61,35 @@ namespace BrutalStory
                 bloodSprayMaxSize, 
                 EnumParticleModel.Cube );
 
+            //Create Wound Bleed Settings
+            //This is just blood that drips out of the wound.
+            Vec3f woundBleedMinVel = new Vec3f(5.0f, 5.0f, 5.0f);
+            Vec3f woundBleedMaxVel = new Vec3f(15.0f, 15.0f, 15.0f);
+            int woundBleedColorCode = bloodColorDefaultCode;
+
+            int woundBleedMinQuantity = 3;
+            int woundBleedMaxQuantity = 8;
+
+            float woundBleedMinVelocityScale = 0f;
+            float woundBleedMaxVelocityScale = 0.1f;
+
+            float woundBleedLifeLength = 10f;
+            float woundBleedGravityEffect = 1.0f;
+
+            float woundBleedMinSize = 1f;
+            float woundBleedMaxSize = 1.5f;
+
+            woundBleedProperties = new BrutalStandardFxData(
+                woundBleedMinQuantity,
+                woundBleedMaxQuantity,
+                woundBleedColorCode,
+                woundBleedMinVelocityScale,
+                woundBleedMaxVelocityScale,
+                woundBleedLifeLength,
+                woundBleedGravityEffect,
+                woundBleedMinSize,
+                woundBleedMaxSize,
+                EnumParticleModel.Cube);
 
             //Create Bloody Water Settings.
             int bloodyWaterMinQuantity = 5;
@@ -80,10 +112,35 @@ namespace BrutalStory
         public static void Bleed( EntityAgent agent, DamageSource damageSource, float damage)
         {
             Debug.Assert(agent.Api.Side == EnumAppSide.Client, "Bleed is being called on the server, this should never happen!");
-
+            
             if (!ShouldPlayBloodFX(agent, damageSource))
                 return;
 
+            SpawnBloodFXForDamage(agent, damageSource, damage);
+        }
+    
+        private static void SpawnBloodFXForDamage(EntityAgent agent, DamageSource damageSource, float damage)
+        {
+            if ( damageSource.Type == EnumDamageType.Poison )
+            {
+                //To Do: have the player barf blood.
+                return;
+            }
+
+            if ( agent.FeetInLiquid ) 
+            {
+                CreateBloodyWaterFX(agent, damageSource, damage);
+            }
+            
+            if ( !agent.Swimming ) 
+            { 
+                CreateBloodSpray(agent, damageSource, damage);
+                CreateWoundBleed(agent, damageSource, damage);
+            }
+        }
+
+        private static void CreateBloodSpray( EntityAgent agent, DamageSource damageSource, float damage) 
+        {
             ITreeAttribute treeAttribute = agent.WatchedAttributes.GetTreeAttribute("health");
 
             Debug.Assert(treeAttribute != null);
@@ -94,8 +151,8 @@ namespace BrutalStory
 
             Vec3d hitPos = GetHitPosition(agent, damageSource);
 
-            Vec3d minPos = agent.Pos.XYZ.Add(hitPos);
-            Vec3d maxPos = agent.Pos.XYZ.Add(hitPos);
+            Vec3d minPos = agent.Pos.XYZ + hitPos;
+            Vec3d maxPos = agent.Pos.XYZ + hitPos;
 
             Entity attacker = damageSource.GetCauseEntity();
 
@@ -106,7 +163,7 @@ namespace BrutalStory
 
             SimpleParticleProperties bloodSprayParticleEffect = new SimpleParticleProperties(
                 quantity,
-                quantity, 
+                quantity,
                 bloodSprayProperties.color,
                 minPos,
                 maxPos,
@@ -121,21 +178,83 @@ namespace BrutalStory
             bloodSprayParticleEffect.ShouldDieInLiquid = true;
 
             agent.World.SpawnParticles(bloodSprayParticleEffect, null);
+        }
 
-            //Test Bloody Water Particles
-            BrutalFloatingBloodParticles bloodyWaterParticleEffect = new BrutalFloatingBloodParticles(
-                bloodyWaterProperties.minQuantity,
-                bloodyWaterProperties.color,
-                emptyColor,
+        private static Vec3d minWoundOffsetVec = new Vec3d(0.05, 0.05, 0.05);
+        private static Vec3d maxWoundOffsetVec = new Vec3d(-0.05, -0.05, -0.05);
+        private static void CreateWoundBleed(EntityAgent agent, DamageSource damageSource, float damage)
+        {
+            ITreeAttribute treeAttribute = agent.WatchedAttributes.GetTreeAttribute("health");
+
+            Debug.Assert(treeAttribute != null);
+            float maxHealth = treeAttribute.GetFloat("maxhealth");
+
+            //Scale Amount of blood by damage compared to max health.
+            int quantity = (int)MathUtility.GraphClampedValue(0, maxHealth, woundBleedProperties.minQuantity, woundBleedProperties.maxQuantity, ((double)damage));
+
+            Vec3d hitPos = GetHitPosition(agent, damageSource);
+
+            Vec3d minPos = agent.Pos.XYZ + (hitPos + minWoundOffsetVec);
+            Vec3d maxPos = agent.Pos.XYZ + (hitPos + maxWoundOffsetVec);
+
+            Entity attacker = damageSource.GetCauseEntity();
+
+            Vec3f hitDir = GetHitDirection(agent, damageSource);
+
+            Vec3f minVelocity = (hitDir * woundBleedProperties.minVelocityScale);
+            Vec3f maxVelocity = (hitDir * woundBleedProperties.maxVelocityScale);
+
+            SimpleParticleProperties bloodSprayParticleEffect = new SimpleParticleProperties(
+                quantity,
+                quantity,
+                woundBleedProperties.color,
                 minPos,
+                maxPos,
+                minVelocity *= -1,
+                maxVelocity *= -1,
+                woundBleedProperties.lifeLength,
+                woundBleedProperties.gravityEffect,
+                woundBleedProperties.minSize,
+                woundBleedProperties.maxSize,
+                woundBleedProperties.model);
+
+            bloodSprayParticleEffect.ShouldDieInLiquid = true;
+
+            agent.World.SpawnParticles(bloodSprayParticleEffect, null);
+        }
+
+        private static Vec3d feetInWaterOffsetVec = new Vec3d(0, 0.5, 0);
+        private static void CreateBloodyWaterFX(EntityAgent agent, DamageSource damageSource, float damage)
+        {        
+            ITreeAttribute treeAttribute = agent.WatchedAttributes.GetTreeAttribute("health");
+
+            Debug.Assert(treeAttribute != null);
+            float maxHealth = treeAttribute.GetFloat("maxhealth");
+
+            //Scale Amount of blood by damage compared to max health.
+            int quantity = (int)MathUtility.GraphClampedValue(0, maxHealth, bloodyWaterProperties.minQuantity, bloodyWaterProperties.maxQuantity, ((double)damage));
+
+            Vec3d hitPos = GetHitPosition(agent, damageSource);
+
+            Vec3d pos = agent.Swimming ? agent.Pos.XYZ + hitPos : agent.Pos.XYZ + feetInWaterOffsetVec;
+
+            Block liquidBlock = agent.World.BlockAccessor.GetBlock(pos.AsBlockPos);
+            //Debug.Assert(liquidBlock.BlockMaterial == EnumBlockMaterial.Liquid);
+            int waterColor = liquidBlock.GetColor(BrutalBroadcast.clientCoreApi, pos.AsBlockPos);
+
+            //Bloody Water Particles
+            BrutalFloatingBloodParticles bloodyWaterParticleEffect = new BrutalFloatingBloodParticles(
+                quantity,
+                bloodyWaterProperties.color,
+                waterColor,
+                pos,
                 bloodyWaterProperties.addPos,
                 bloodyWaterProperties.addVelocity
                 );
 
             agent.World.SpawnParticles(bloodyWaterParticleEffect, null);
-
         }
-    
+
         private static Vec3f emptyHitDir = new Vec3f(0f, 0f, 0f);
         private static Vec3f upwardHitDir = new Vec3f(0f, -1f, 0f);
         private static Vec3f GetHitDirection( EntityAgent agent, DamageSource damageSource )
@@ -150,7 +269,10 @@ namespace BrutalStory
                 //Fall and Splat, do a blood geiser.
                 case EnumDamageType.Gravity:
                 {
-                    return upwardHitDir; 
+                    float xRand = agent.World.Rand.NextDouble() > 0.5 ? (float)(agent.World.Rand.NextDouble() * 0.05) : (float)(agent.World.Rand.NextDouble() * -0.05);
+                    float yRand = agent.World.Rand.NextDouble() > 0.5 ? (float)(agent.World.Rand.NextDouble() * 0.05) : (float)(agent.World.Rand.NextDouble() * -0.05);
+                    Vec3f upSplatterVec = new Vec3f(xRand, upwardHitDir.Y, yRand);
+                    return upSplatterVec; 
                 }
             }
 
@@ -159,7 +281,7 @@ namespace BrutalStory
                 if (attacker is EntityAgent)
                 {
                     Vec3d hitPos = GetHitPosition( agent, damageSource );
-                    hitDir = ( agent.Pos.XYZ.Add(hitPos) - attacker.Pos.XYZ.Add(attacker.LocalEyePos) ).ToVec3f().Normalize();
+                    hitDir = ((agent.Pos.XYZ + hitPos) - (attacker.Pos.XYZ + attacker.LocalEyePos)).ToVec3f().Normalize();
                 }
             }
 
@@ -168,7 +290,7 @@ namespace BrutalStory
 
         private static Vec3d GetHitPosition(  EntityAgent agent, DamageSource damageSource )
         {
-            return damageSource.HitPosition != null ? damageSource.HitPosition : agent.LocalEyePos.Mul(0.75);
+            return damageSource.HitPosition != null ? damageSource.HitPosition : agent.LocalEyePos * 0.75;
         }
 
         private static bool ShouldPlayBloodFX( EntityAgent agent, DamageSource damageSource )
@@ -185,6 +307,7 @@ namespace BrutalStory
                     case EnumDamageType.BluntAttack:
                     case EnumDamageType.Crushing:
                     case EnumDamageType.SlashingAttack:
+                    case EnumDamageType.Poison:
                         return true;
 
                 }
--- mods-dll/brutalstory/src/BrutalStoryCore.cs
+++ mods-dll/brutalstory/src/BrutalStoryCore.cs
@@ -12,30 +12,6 @@ using Vintagestory.GameContent;
 
 namespace BrutalStory
 {
-    [ProtoContract(ImplicitFields = ImplicitFields.AllPublic)]
-    public class BrutalDamagePacket
-    {
-        public long victimEntityID;
-
-        public EnumDamageSource Source;
-        public EnumDamageType Type;
-        public Vec3d HitPosition;
-        public long SourceEntityID;
-        public long CauseEntityID;
-       // public BlockPos SourceBlockPos;
-        public Vec3d SourcePos;
-        public int DamageTier = 0;
-        public float KnockbackStrength = 1f;
-
-        public float damage;
-    }
-
-    [ProtoContract(ImplicitFields = ImplicitFields.AllPublic)]
-    public class BrutalNetworkTest
-    {
-
-    }
-
     public class BrutalStoryModCore : ModSystem
     {
 
@@ -68,8 +44,6 @@ namespace BrutalStory
             base.StartServerSide(api);
 
             Debug.Assert( api.Network.GetChannel( "brutalPacket" ) != null );
-
-            api.RegisterCommand("brutaltest", "Send a test network message", "", OnBrutalTestCmd, Privilege.controlserver);
         }
 
         private void OnBrutalTestCmd(IServerPlayer player, int groupId, CmdArgs args)
@@ -91,64 +65,10 @@ namespace BrutalStory
             Debug.Assert(api.Network.GetChannel("brutalPacket") != null);
 
             api.Network.GetChannel( "brutalPacket" )
-                .SetMessageHandler<BrutalDamagePacket>(HandleDamagePacket)
-                //.SetMessageHandler<BrutalNetworkTest>(HandleNetworkTest)
+                .SetMessageHandler<BrutalDamagePacket>(BrutalBroadcast.HandleDamagePacket)
+                //.SetMessageHandler<BrutalNetworkTest>(BrutalBroadcast.HandleNetworkTest)
                 ;
 
-            api.RegisterCommand("clienttest", "Send a test network message", "", OnBrutalClientTestCmd );
-
-        }
-
-        private void HandleDamagePacket(BrutalDamagePacket networkMessage)
-        {
-            Entity victimEntity = null;
-            Entity sourceEntity = null;
-            Entity causeEntity = null;
-            //Block sourceBlock = null;
-
-            EntityAgent victimAgent = null;
-            if ( networkMessage.victimEntityID != -1 )
-            {
-                victimEntity = BrutalBroadcast.clientCoreApi.World.GetEntityById(networkMessage.victimEntityID);
-
-                if (victimEntity == null)
-                    return;
-
-                Debug.Assert( victimEntity is EntityAgent );
-                victimAgent = (EntityAgent) victimEntity;
-            }
-                
-
-            if ( networkMessage.SourceEntityID != -1 ) 
-                sourceEntity = BrutalBroadcast.clientCoreApi.World.GetEntityById( networkMessage.SourceEntityID );
-
-            if ( networkMessage.CauseEntityID != -1 )
-                causeEntity = BrutalBroadcast.clientCoreApi.World.GetEntityById( networkMessage.CauseEntityID );
-
-            //sourceBlock = BrutalBroadcast.clientCoreApi.World.BlockAccessor.GetBlock(networkMessage.SourceBlockPos);
-
-            DamageSource damageSource       = new DamageSource();
-            damageSource.Source             = networkMessage.Source;
-            damageSource.Type               = networkMessage.Type;
-            damageSource.HitPosition        = networkMessage.HitPosition;
-            damageSource.SourceEntity       = sourceEntity;
-            damageSource.CauseEntity        = causeEntity;
-            //damageSource.SourceBlock        = sourceBlock;
-            damageSource.SourcePos          = networkMessage.SourcePos;
-            damageSource.DamageTier         = networkMessage.DamageTier;
-            damageSource.KnockbackStrength  = networkMessage.KnockbackStrength;
-
-            BloodFX.Bleed(victimAgent, damageSource, networkMessage.damage);
-        }
-
-        private void HandleNetworkTest(BrutalNetworkTest networkMessage) 
-        {
-            BrutalBroadcast.clientCoreApi.ShowChatMessage("BRUTAL MESSAGE RECIEVED FROM BRUTAL SERVER" );
-        }
-
-        private void OnBrutalClientTestCmd( int groupId, CmdArgs args)
-        {
-            ICoreClientAPI capi = BrutalBroadcast.clientCoreApi;
         }
 
         #endregion //Client
